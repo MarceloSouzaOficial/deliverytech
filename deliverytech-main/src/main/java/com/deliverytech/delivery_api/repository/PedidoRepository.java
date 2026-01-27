e83e47650480957295779671f317ccb89bb2d5af
Marcelo <marcelo_rib.souza@hotmail.com>
2026-01-26 23:38:13 -0300

    Enviando minha apliação para o remoto

---
+++ deliverytech-main/src/main/java/com/deliverytech/delivery_api/repository/PedidoRepository.java
@@ -0,0 +1,52 @@
+package com.deliverytech.delivery_api.repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.deliverytech.delivery_api.dto.TotalVendasPorRestauranteDTO;
+import com.deliverytech.delivery_api.enums.StatusPedidos;
+import com.deliverytech.delivery_api.model.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+    List<Pedido> findByClienteId(Long clienteId);
+
+    List<Pedido> findByStatus(StatusPedidos status);
+
+    @Query("""
+            SELECT p FROM Pedido p
+            WHERE p.dataPedido  BETWEEN :inicio AND :fim
+    """)
+    List<Pedido> findByDateTime(
+        @Param("inicio") LocalDateTime inicio,
+        @Param("fim") LocalDateTime fim
+    );
+
+
+        @Query("""
+            select new com.deliverytech.delivery_api.dto.TotalVendasPorRestauranteDTO(
+                    r.nome,
+                    coalesce(sum(ip.subtotal), 0)
+                )
+                from Pedido p
+                join p.restaurante r
+                join p.itens ip
+                group by r.nome
+            """)
+            List<TotalVendasPorRestauranteDTO> totalVendasPorRestaurante();
+
+            @Query(value="""
+                        SELECT c.nome AS cliente, COUNT(p.id) AS total_pedidos
+                        FROM pedidos p 
+                        JOIN clientes c ON c.id = p.cliente_id
+                        GROUP BY c.nome
+                        ORDER BY total_pedidos DESC
+                """, nativeQuery = true )
+            List<Object[]> rankingClientes();
+    
+}
