5e90444e358cd10c43ba84fed32d4cb32c3d4797
Marcelo <marcelo_rib.souza@hotmail.com>
2026-01-29 20:38:38 -0300

    Atualização da Aplicação

---
+++ src/main/java/com/deliverytech/delivery_api/service/RelatorioService.java
@@ -0,0 +1,25 @@
+package com.deliverytech.delivery_api.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.deliverytech.delivery_api.dto.TotalVendasPorRestauranteDTO;
+import com.deliverytech.delivery_api.repository.PedidoRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class RelatorioService {
+    private final PedidoRepository respository;
+    
+    public List<TotalVendasPorRestauranteDTO> totalVendasPorRestaurante(){
+        return respository.totalVendasPorRestaurante();
+    }  
+
+    public List<Object[]> rankingClientes(){
+        return respository.rankingClientes();
+    }
+
+}
