3212e2c8ebb2c4bd53c68f28c4603887bd7e2886
Marcelo <marcelo_rib.souza@hotmail.com>
2026-02-04 20:10:52 -0300

    DTO's requisições e respostas de produto e temProduto foram criados

---
+++ src/main/java/com/deliverytech/delivery_api/dto/requests/PedidoDTO.java
@@ -0,0 +1,28 @@
+package com.deliverytech.delivery_api.dto.requests;
+
+import java.util.List;
+
+import com.deliverytech.delivery_api.model.ItemPedido;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PedidoDTO {
+    @NotBlank
+    private String enderecoEntrega;
+
+    @NotNull
+    private Long clienteId;
+
+    @NotNull
+    private Long restauranteId;
+
+    @Valid
+    @NotNull
+    private List<ItemPedido> itens;
+}
